74029434b893efed22c9fcd7f693c2a14b25704a
Phil Allen <phil.allen@phlall.ndo.co.uk>
2021-08-12 15:29:05 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="nav">
+        <navTop/>
+        <navBar/>
+    <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
+  </div>
+  <router-view />
+</template>
+<script>
+import navTop from "./components/NavTop.vue";
+import navBar from "./components/NavBar.vue";
+
+export default {
+  components: {
+   navBar,
+   navTop
+  }
+}
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "@/assets/styles/tailwind.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faTwitter, faFacebookSquare, faLinkedin, faPinterestSquare, faVimeo} from "@fortawesome/free-brands-svg-icons";
+library.add(faPhone, faEnvelope, faTwitter, faFacebookSquare, faLinkedin, faPinterestSquare, faVimeo);
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+createApp(App).use(store).use(router).use(library).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
